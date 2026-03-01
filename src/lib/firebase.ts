4dc655639d76e4a15320dcfd96b8bb771197439e
Alvin11David <alvin69david@gmail.com>
2026-03-01 11:45:33 +0300

    Changed to firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUK60Asm7pgjm0F1P2Lm11gHxJRgZya0g",
+  authDomain: "institution-portal.firebaseapp.com",
+  projectId: "institution-portal",
+  storageBucket: "institution-portal.firebasestorage.app",
+  messagingSenderId: "401202457232",
+  appId: "1:401202457232:web:2339107b190eab6f764615",
+  measurementId: "G-CG8GKC42S3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
