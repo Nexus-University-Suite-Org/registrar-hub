1b29c75bc9bd5cf0b83feb48bafdc5008077a14b
Alvin11David <alvin69david@gmail.com>
2026-03-01 11:51:06 +0300

    Changed to firebase

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -39,11 +39,9 @@ export default function Reports() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const checkAuth = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      if (!session) {
+    const checkAuth = () => {
+      const user = auth.currentUser;
+      if (!user) {
         navigate("/");
       }
     };
