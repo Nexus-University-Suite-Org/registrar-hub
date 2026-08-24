bcbd713793635d4d12bb45d57dca20e726999579
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-08-24 13:40:56 +0300

    Add dependencies for project

---
+++ REG-Backend/src/main/java/org/nexus/regbackend/RegBackendApplication.java
@@ -0,0 +1,13 @@
+package org.nexus.regbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegBackendApplication.class, args);
+    }
+
+}
---
+++ REG-Backend/src/test/java/org/nexus/regbackend/RegBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexus.regbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
