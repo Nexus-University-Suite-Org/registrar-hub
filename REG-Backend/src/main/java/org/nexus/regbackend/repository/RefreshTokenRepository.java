a2e2e4409e1e2983115f919ed2f5c9c717e64317
sekibaalamark <markmajje256@gmail.com>
2026-08-27 19:13:27 +0300

    created refresh token repository

---
+++ REG-Backend/src/main/java/org/nexus/regbackend/repository/RefreshTokenRepository.java
@@ -0,0 +1,19 @@
+package org.nexus.regbackend.repository;
+
+import org.nexus.regbackend.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM RefreshToken r WHERE r.registrar.id = :registrarId")
+    void deleteAllByRegistrarId(Long registrarId);
+}
